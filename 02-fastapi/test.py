2cbfb50ec1342d41394b096c6c3d8f576f419728
Pablo Costa <92pablocosta@gmail.com>
2026-07-08 18:05:35 -0300

    feat: add FastAPI application with product listing endpoint

---
+++ 02-fastapi/test.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/products")
+def list_products(
+    category: str | None = None,
+    limit: int = 10
+):
+    if category is None:
+        return {
+            "message": "Listing products from all categories",
+            "limit": limit
+        }
+
+    return {
+        "message": f"Listing products from category: {category}",
+        "limit": limit
+    }
