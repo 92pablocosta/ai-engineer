5d25a9c0a71725cbb6f4c1171f9dc60cbfcef33c
Pablo Costa <92pablocosta@gmail.com>
2026-07-15 20:04:45 -0300

    fix: update checkCpfDigit() and validateCPF().

--- 00-python/foundation_projects/01-cpf_cnpj_validator/functions.py
+++ 00-python/foundation_projects/01-cpf_cnpj_validator/functions.py
@@ -1,11 +1,47 @@
+def checkCpfDigit(cpf: str):
+    add = 0
+    first_digit = 0
+    second_digit = 0
+
+    for i in range(9):
+        add += int(cpf[i]) * (10 - i)
+
+    rest = add % 11
+
+    if rest < 2:
+        first_digit = 0
+    else:
+        first_digit = 11 - rest
+
+    add = 0
+    for i in range(10):
+        if (11 - i) == 2:
+            add += first_digit * 2
+        else:
+            add += int(cpf[i]) * (11 - i)
+
+    rest = add % 11
+
+    if rest < 2:
+        second_digit = 0
+    else:
+        second_digit = 11 - rest
+
+    if int(cpf[-2]) == first_digit and int(cpf[-1]) == second_digit:
+        return True
+    else:
+        return False
+
+
 def validateCPF(cpf: str):
     cpf = cpf.replace(".", "").replace("-", "")
+    if len(cpf) != 11:
+        return False, 'Invalid CPF'
     if cpf.isdigit():
-        return 'CPF Válido'
+        return True, 'CPF Válido'
     else:
-        return 'CPF inválido'
-    
+        return False, 'CPF inválido'
+
 
 def validateCNPJ(cnpj: str):
     pass
-
--- 00-python/foundation_projects/01-cpf_cnpj_validator/main.py
+++ 00-python/foundation_projects/01-cpf_cnpj_validator/main.py
@@ -1,4 +1,35 @@
-from functions import *
+cpf = "09575980417"
 
-cpf = str(input("Type your CPF: "))
-print(validateCPF(cpf))
+add = 0
+first_digit = 0
+second_digit = 0
+
+for i in range(9):
+    add += int(cpf[i]) * (10 - i)
+
+
+rest = add % 11
+
+if rest < 2:
+    first_digit = 0
+else:
+    first_digit = 11 - rest
+
+add = 0
+for i in range(10):
+    if (11 - i) == 2:
+        add += first_digit * 2
+    else:
+        add += int(cpf[i]) * (11 - i)
+
+rest = add % 11
+
+if rest < 2:
+    second_digit = 0
+else:
+    second_digit = 11 - rest
+
+if int(cpf[-2]) == first_digit and int(cpf[-1]) == second_digit:
+    print('This CPF is VALID.')
+else:
+    print('Invalid CPF.')
